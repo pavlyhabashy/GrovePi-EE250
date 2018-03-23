19a6da30a94264405790490c34edaae31abd840a
Pavly Habashy <Pavly@Pavlys-MBP.lan>
2018-03-23 16:10:48 -0700

    Fixed

--- ee250/lab07/vm_subscriber.py
+++ ee250/lab07/vm_subscriber.py
@@ -7,7 +7,7 @@ import time
 
 
 def ultrasonic_callback(client, userdata, message):
-    print(str(message.payload))
+    print(str(message.payload, "utf-8"))
 
 def button_callback(client, userdata, message):
     print(str(message.payload, "utf-8"))
@@ -16,19 +16,19 @@ def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
     client.subscribe("anrg-pi7/ultrasonicRanger")
-    client.message_callback_add("anrg-pi7/led", ultrasonic_callback)
+    client.message_callback_add("anrg-pi7/ultrasonicRanger", ultrasonic_callback)
 
     client.subscribe("anrg-pi7/button")
-    client.message_callback_add("anrg-pi7/led", button_callback)
+    client.message_callback_add("anrg-pi7/button", button_callback)
 
 #Default message callback. Please use custom callbacks.
-# def on_message(client, userdata, msg):
-#     print("on_message: " + msg.topic + " " + str(msg.payload))
+def on_message(client, userdata, msg):
+    print("on_message: " + msg.topic + " " + str(msg.payload))
 
 if __name__ == '__main__':
     #this section is covered in publisher_and_subscriber_example.py
     client = mqtt.Client()
-    # client.on_message = on_message
+    client.on_message = on_message
     client.on_connect = on_connect
     client.connect(host="eclipse.usc.edu", port=11000, keepalive=60)
     client.loop_start()
